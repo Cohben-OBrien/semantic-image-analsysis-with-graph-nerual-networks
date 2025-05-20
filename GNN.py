ffc36fafae11e1deb965834839af5f117a175c8e
Cohben O'Brien <26069199@edgehill.ac.uk>
2025-05-20 10:13:56 +0100

    expermimenting with gnn model and passing projections and knns using tfgnn

--- GNN.py
+++ GNN.py
@@ -3,121 +3,175 @@ from keras import layers
 import tensorflow as tf
 from networkx.classes import neighbors
 
-
 def ffn(hidden_units, dropout_rate, name=None):
     fnn_layers = []
 
     for units in hidden_units:
         fnn_layers.append(layers.BatchNormalization())
         fnn_layers.append(layers.Dropout(dropout_rate))
-        fnn_layers.append(layers.Dense(units, activation='relu'))
+        fnn_layers.append(layers.Dense(units, activation ='gelu'))
 
-    return keras.Sequential(fnn_layers, name=name)
-
-
-def create_baseline_model(hidden_units, num_classes, dropout_rate):
-    num_features = hidden_units[0]
-    inputs = keras.Input(shape=(num_features, ), name='input_features')
-    x = ffn(hidden_units, dropout_rate, name='ffn_block1')(inputs)
-
-    for block_idx in range(4):
-        x1 = ffn(hidden_units, dropout_rate, name=f'ffn_block{block_idx+2}')(x)
-        x = layers.Add(name=f'skip_connection{block_idx + 2}')([x, x1])
-
-    logits = layers.Dense(num_classes, name='logits')(x)
-
-    return keras.Model(inputs=inputs, outputs=logits, name='baseline_model')
+    return keras.Sequential(fnn_layers,
+                            name=name)
 
 def create_gru(hidden_units, dropout_rate):
     inputs = keras.layers.Input(shape=(2, hidden_units[0]))
     x = inputs
 
+
     for units in hidden_units:
         x = layers.GRU(
-            units,
+            units=units,
             activation='tanh',
             recurrent_activation='sigmoid',
             return_sequences=True,
-            droupout=dropout_rate,
+            dropout=dropout_rate,
             return_state=False,
-            recurrent_dropout=dropout_rate
+            recurrent_dropout=dropout_rate,
+
         )(x)
 
     return keras.Model(inputs=inputs, outputs=x)
 
-class GrahnConvLayer(layers.Layer):
+class GraphConvLayer(layers.Layer):
     def __init__(self,
                  hidden_units,
                  dropout_rate=0.2,
-                 aggregation_type='mean',
+                 aggregation='mean',
                  combination_type='concat',
-                 normalize=False,
+                 normalization=False,
                  *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.aggregation_type = aggregation_type
+        self.aggregation = aggregation
         self.combination_type = combination_type
-        self.normalize = normalize
+        self.normalization = normalization
 
-        self.fnn_prepare = ffn(hidden_units, dropout_rate)
+        self.ffn_prepare = ffn(hidden_units, dropout_rate)
+        if self.combination_type == 'gru':
+            self.update_fn = create_gru(hidden_units, dropout_rate)
+        else:
+            self.update_fn = ffn(hidden_units, dropout_rate)
+
+
+    def prepare(self, node_representation, weights=None):
+        messages = self.ffn_prepare(node_representation)
+
+        if weights is not None:
+            messages = messages * tf.expand_dims(weights, axis=-1)
+        return messages
+
+    def aggregate(self, node_indices, neighbour_messages, node_representation):
+        num_nodes = node_representation.shape[0]
+        print(self.aggregation)
+        if self.aggregation == "sum":
+            aggregated_messages = tf.math.unsorted_segment_sum(
+                neighbour_messages, node_indices, num_segments=num_nodes
+            )
+        elif self.aggregation == "mean":
+            aggregated_messages = tf.math.unsorted_segment_mean(
+                neighbour_messages, node_indices, num_segments=num_nodes
+            )
+        elif self.aggregation == "max":
+            aggregated_messages = tf.math.unsorted_segment_max(
+                neighbour_messages, node_indices, num_segments=num_nodes
+            )
+        else: raise ValueError(f'Unknown aggregation type: {self.aggregation}')
+
+        return aggregated_messages
+
+    def update(self, node_repesemtation, aggregated_messages):
 
         if self.combination_type == 'gru':
-            self.update_fm = create_gru(hidden_units, dropout_rate)
+            #create a sequence of two elements for the GRU layer
+            h = tf.stack([node_repesemtation, aggregated_messages], axis=1)
+        elif self.combination_type == 'concat':
+            h = tf.concat([node_repesemtation, aggregated_messages], axis=1)
+        elif self.combination_type == 'add':
+            h = node_repesemtation + aggregated_messages
         else:
-            self.update_fm = ffn(hidden_units, dropout_rate)
+            raise ValueError(f'Unknown combination type: {self.combination_type}')
 
-        def prepare(self, node_rep, weights=None):
-            mesages = self.fnn_prepare(node_rep)
-            if weights is not None:
-                messages = self.fnn_prepare * tf.expand_dims(weights, -1)
-            return messages
+        node_embeding = self.update_fn(h)
+        if self.combination_type == 'gru':
+            node_embeding = tf.unstack(node_embeding, axis=1)[-1]
 
-        def aggregate(self, node_indices, neigbour_messages, node_reps):
-            num_nodes = node_reps.shape[0]
+        if self.normalization:
+            node_embeding = tf.nn.l2_normalize(node_embeding, axis=1)
+        return node_embeding
 
-            if self.aggregation_type == 'sum':
-                aggregated_messages = tf.math.unsorted_segment_sum(
-                    neigbour_messages, node_indices, num_segments=num_nodes
-                )
-            elif self.aggregation_type == 'mean':
-                aggregation_messages = tf.math.unsorted_segment_mean(
-                    neigbour_messages, node_indices, num_segments=num_nodes
-                )
-            elif self.aggregation_type == 'max':
-                aggregation_messages = tf.math.unsorted_segment_max(
-                    neigbour_messages, node_indices, num_segments=num_nodes
-                )
-            else:
-                raise ValueError(f'invalid aggregation type: {self.aggregation_type}')
+    def call(self, inputs):
+        node_repesentation, edges, edge_weights = inputs
 
-        def update(self, node_rep, aggregated_messages):
-            if self.combination_type == 'gru':
-                h = tf.stack([node_rep, aggregated_messages], axis=1)
-            elif self.combination_type == 'concat':
-                h = tf.concat([node_rep, aggregated_messages], axis=1)
-            elif combination_type == 'add':
-                h = node_rep + aggregated_messages
-            else:
-                raise ValueError(f'invalid combination type: {self.combination_type}')
+        node_indices, neighbors_indices = edges[0], edges[1]
 
-            node_embedding = self.update_fnn(h)
-            if self.combination_type == 'gru':
-                node_embedding = tf.unstack(node_embedding, axis=1)[-1]
+        neighbour_representation = tf.gather(node_repesentation, neighbors_indices)
 
-            if self.normalize:
-                node_embedding = tf.nn.l2_normalize(node_embedding, axis=-1)
+        neighbour_massages = self.prepare(neighbour_representation, edge_weights)
 
-            return node_embedding
+        aggregated_messages = self.aggregate(
+            node_indices, neighbour_massages, node_repesentation
+        )
 
-        def call(self, inputs):
-            node_reps, edges, edge_weights = inputs
+        return self.update(node_repesentation, aggregated_messages)
 
-            node_indices, neighbors_indices = edges[0], edges[1]
 
-            neighbour_reps = tf.gather(node_reps, neighbors_indices)
 
-            neighbour_messages = self.prepare(node_reps, edge_weights)
-            aggreated_messages = self.aggregate(node_indices, neighbour_messages, node_reps)
+#%%
+class GNNNodeClassification(tf.keras.Model):
+    def __init__(self,
+                 graph_info,
+                 num_classes,
+                 hidden_units,
+                 aggregation_type="mean",
+                 combination_type='concat',
+                 dropout_rate=0.2,
+                 normalize=True,
+                 *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-            return self.update(node_reps, aggreated_messages)
+        node_features, edges, edge_weights = graph_info
+
+        self.node_features = node_features
+        self.edges = edges
+        self.edge_weights = edge_weights
+
+        self.preprocess = ffn(hidden_units, dropout_rate, name=f'preprocess')
+
+        if self.edge_weights is None:
+            self.edge_weights = tf.ones(shape=edges.shape[1])
+
+        self.edge_weights / tf.math.reduce_sum(self.edge_weights)
+
+
+        self.conv1 = GraphConvLayer(hidden_units, dropout_rate,
+                                    aggregation=aggregation_type,
+                                    combination_type=combination_type,
+                                    normalization=normalize,
+                                    name='graph_conv1')
+
+        self.conv2 = GraphConvLayer(hidden_units, dropout_rate,
+                                    aggregation=aggregation_type,
+                                    combination_type=combination_type,
+                                    normalization=normalize,
+                                    name='graph_conv2')
+        self.postprocess = ffn(hidden_units, dropout_rate, name=f'postprocess')
+        self.compute_logits = layers.Dense(num_classes, name='logits')
+
+
+
+    def call(self, input_node_indices):
+        x = self.preprocess(self.node_features)
+        x1 = self.conv1((x, self.edges, self.edge_weights))
+        x = x + x1
+
+        x2 = self.conv2((x, self.edges, self.edge_weights))
+        x = x + x2
+
+        x = self.postprocess(x)
+
+        print(input_node_indices)
+        node_embedding = tf.gather(x, input_node_indices)
+
+        return self.compute_logits(node_embedding)
 
--- RepresentationLearner.py
+++ RepresentationLearner.py
@@ -40,52 +40,48 @@ class RepresentationLearner(keras.Model):
         num_augmentations = keras.ops.shape(feature_vectors)[0] // batch_size
         if self.l2_normalize:
             feature_vectors = keras.utils.normalize(feature_vectors)
+
         # The logits shape is [num_augmentations * batch_size, num_augmentations * batch_size].
         logits = (
                 tf.linalg.matmul(feature_vectors, feature_vectors, transpose_b=True)
                 / self.temperature
         )
+
         # Apply log-max trick for numerical stability.
         logits_max = keras.ops.max(logits, axis=1)
         logits = logits - logits_max
-        # The shape of targets is [num_augmentations * batch_size, num_augmentations * batch_size].
-        # targets is a matrix consits of num_augmentations submatrices of shape [batch_size * batch_size].
-        # Each [batch_size * batch_size] submatrix is an identity matrix (diagonal entries are ones).
+
         targets = keras.ops.tile(
             tf.eye(batch_size), [num_augmentations, num_augmentations]
         )
-        # Compute cross entropy loss
+
         return keras.losses.categorical_crossentropy(
             y_true=targets, y_pred=logits, from_logits=True
         )
 
     def call(self, inputs):
-        # Preprocess the input images.
         preprocessed = processer(inputs)
-        # Create augmented versions of the images.
+
         augmented = []
+
         for _ in range(self.num_augmentations):
             augmented.append(data_augmentation(preprocessed))
         augmented = layers.Concatenate(axis=0)(augmented)
-        # Generate embedding representations of the images.
         features = self.encoder(augmented)
-        # Apply projection head.
         return self.projector(features)
 
     def train_step(self, inputs):
         batch_size = keras.ops.shape(inputs)[0]
-        # Run the forward pass and compute the contrastive loss
         with tf.GradientTape() as tape:
             feature_vectors = self(inputs, training=True)
             loss = self.compute_contrastive_loss(feature_vectors, batch_size)
-        # Compute gradients
+
         trainable_vars = self.trainable_variables
         gradients = tape.gradient(loss, trainable_vars)
-        # Update weights
+
         self.optimizer.apply_gradients(zip(gradients, trainable_vars))
-        # Update loss tracker metric
         self.loss_tracker.update_state(loss)
-        # Return a dict mapping metric names to current value
+
         return {m.name: m.result() for m in self.metrics}
 
     def test_step(self, inputs):
@@ -93,4 +89,4 @@ class RepresentationLearner(keras.Model):
         feature_vectors = self(inputs, training=False)
         loss = self.compute_contrastive_loss(feature_vectors, batch_size)
         self.loss_tracker.update_state(loss)
-        return {"loss": self.loss_tracker.result()}
+        return {"loss": self.loss_tracker.result()  }
